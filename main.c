7ea6d0e3452823bd9dd1947aa62955f8b360e36a
DeepM25 <deepmala242002@gmail.com>
2025-08-16 21:11:27 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+int main(){
+    char operator = '\0';
+    float num1 = 0.0;
+    float num2 = 0.0;
+    float result = 0.0;
+    printf("Enter the operator (+ - * /): ");
+    scanf(" %c",&operator);
+    printf("Enter first number: ");
+    scanf(" %f",&num1);
+    printf("Enter second number: ");
+    scanf(" %f",&num2);
+
+    switch(operator){
+        case '+':
+           result = num1 + num2;
+           printf("%.2f",result);
+           break;
+        case '-':
+           result = num1 - num2;
+           printf("%.2f",result);
+           break;
+        case '*':
+           result = num1 * num2;
+           printf("%.2f",result);
+           break;
+        case '/':
+           result = num1/num2;
+           printf("%.2f",result);
+           break;
+        default:
+            printf("Enter correct operator.");
+    }
+
+    
+
+    
+
+    
+    return 0;
+}
